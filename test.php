59840b7905e1aa8a137808bdf635db396aa03045
alexkong1123 <103373823+alexkong1123@users.noreply.github.com>
2022-05-03 15:09:10 +0800

    php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 $username = "test";
 $password = "12345";
 
-if ($_POST['username'] == $username && $_POST['password'] == $password ) {
+if ($_POST['text'] == $username && $_POST['password'] == $password ) {
 echo "Login Sucessfully.";
 } else {
 echo "Login Failed.";
